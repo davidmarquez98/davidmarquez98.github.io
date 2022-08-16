2e1fc39d071433fa713def4185010509f617f924
David Marquez <davidmarquez2222@outlook.com>
2022-08-16 15:02:59 -0300

    Creacion de funcion para esconder el header al hacer scroll down y primeros cambios en la seccion FORMACION ACADEMICA

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let principalUbication = window.pageYOffset;
+window.onscroll = function(){
+    let actualDisplacement = window.pageYOffset;
+    if(principalUbication >= actualDisplacement){
+        document.getElementById("id_header").style.top = '0';
+    }else{
+        document.getElementById("id_header").style.top = '-100px';
+    }
+    principalUbication = actualDisplacement;
+}
